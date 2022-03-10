73ee2317cba7f2fbf770075c4fd8f2f1d6bb986b
Carles Capellas <capellas.carles@gmail.com>
2022-03-10 13:20:03 +0100

    Define training plan

--- source/index.tsx
+++ source/index.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { plan } from './plan';
+import { TrainingType } from './types';
 
 const App: React.FC = () => {
-  return <h1>Marathon planner</h1>;
+  return (
+    <div>
+      <h1>Marathon planner</h1>
+      {plan.map((week) => {
+        return (
+          <div>
+            <h3>Week {week.number}</h3>
+            <div className="week" style={{ display: 'flex' }}>
+              {week.trainings.map((training) => {
+                return (
+                  <div className="day" style={{ textAlign: 'center', width: '14.28%' }}>
+                    <div>{training.type}</div>
+                    {(training.type === TrainingType.comfortable ||
+                      training.type === TrainingType.recovery ||
+                      training.type === TrainingType.timed) && <div>{training.distance} miles</div>}
+                    {(training.type === TrainingType.speed ||
+                      training.type === TrainingType.strength) && (
+                      <div>
+                        <div>
+                          {training.intervalsNumber}x {training.intervalDistance} miles
+                        </div>
+                        <div>Recovery: {training.intervalRecovery} miles</div>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 const appPlaceholder = document.getElementById('app-placeholder');
---
+++ source/types.ts
@@ -0,0 +1,29 @@
+export type Training =
+  | {
+      type: TrainingType.comfortable | TrainingType.recovery | TrainingType.timed;
+      distance: number;
+    }
+  | {
+      type: TrainingType.race | TrainingType.rest;
+    }
+  | {
+      type: TrainingType.speed | TrainingType.strength;
+      intervalDistance: number;
+      intervalRecovery: number;
+      intervalsNumber: number;
+    };
+
+export enum TrainingType {
+  comfortable = 'comfortable',
+  recovery = 'recovery',
+  race = 'race',
+  rest = 'rest',
+  speed = 'speed',
+  strength = 'strength',
+  timed = 'timed'
+}
+
+export type Week = {
+  number: number;
+  trainings: Training[];
+};
