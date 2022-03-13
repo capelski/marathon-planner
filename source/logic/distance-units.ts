a89147a5c815f06512b5ad92fe3e407e86406aba
Carles Capellas <capellas.carles@gmail.com>
2022-03-13 10:41:11 +0100

    Round converted distances down

--- source/logic/distance-units.ts
+++ source/logic/distance-units.ts
@@ -1,4 +1,4 @@
 import { DistanceUnits } from '../models';
 
 export const convertDistance = (distance: number, distanceUnits: DistanceUnits) =>
-  distanceUnits === DistanceUnits.kilometers ? Math.round(distance * 16.09) / 10 : distance;
+  distanceUnits === DistanceUnits.kilometers ? Math.floor(distance * 16.09) / 10 : distance;
