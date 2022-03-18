23a0cd2abea5d0028074995db5a4171144e128e9
Carles Capellas <capellas.carles@gmail.com>
2022-03-18 15:00:15 +0100

    Handle week collapsing from plan component

--- source/components/plan.tsx
+++ source/components/plan.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { DetailedPlan } from '../types';
+import React, { useEffect, useState } from 'react';
+import { DetailedPlan, Dictionary } from '../types';
 import { Week } from './week';
 
 export interface PlanProps {
@@ -7,12 +7,40 @@ export interface PlanProps {
   plan: DetailedPlan;
 }
 
+const collapsedWeeksKey = 'collapsedWeeks';
+
 export const Plan: React.FC<PlanProps> = (props) => {
+  const [collapsedWeeks, setCollapsedWeeks] = useState<Dictionary<boolean, number>>({});
+
+  useEffect(() => {
+    const _collapsedWeeks = localStorage.getItem(collapsedWeeksKey);
+    if (_collapsedWeeks) {
+      setCollapsedWeeks(JSON.parse(_collapsedWeeks));
+    }
+  }, []);
+
+  const changeCollapseStatus = (weekNumber: number) => {
+    const nextCollapsedWeeks = {
+      ...collapsedWeeks,
+      [weekNumber]: !collapsedWeeks[weekNumber]
+    };
+    setCollapsedWeeks(nextCollapsedWeeks);
+    localStorage.setItem(collapsedWeeksKey, JSON.stringify(nextCollapsedWeeks));
+  };
+
   return (
     <React.Fragment>
       <h2>Plan</h2>
       {props.plan.map((week) => {
-        return <Week isDesktop={props.isDesktop} key={week.number} week={week} />;
+        return (
+          <Week
+            isCollapsed={collapsedWeeks[week.number]}
+            isDesktop={props.isDesktop}
+            key={week.number}
+            toggleIsCollapsed={() => changeCollapseStatus(week.number)}
+            week={week}
+          />
+        );
       })}
     </React.Fragment>
   );
--- source/components/week.tsx
+++ source/components/week.tsx
@@ -6,41 +6,26 @@ import { Time } from './time';
 import { Training } from './training';
 
 export interface WeekProps {
+  isCollapsed?: boolean;
   isDesktop: boolean;
+  toggleIsCollapsed: () => void;
   week: DetailedWeek;
 }
 
 export const Week: React.FC<WeekProps> = (props) => {
-  const weekKey = `is-week-${props.week.number}-collapsed`;
-
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  useEffect(() => {
-    const _isCollapsed = localStorage.getItem(weekKey);
-    if (_isCollapsed) {
-      setIsCollapsed(JSON.parse(_isCollapsed));
-    }
-  }, []);
-
-  const changeCollapseStatus = () => {
-    const nextIsCollapsed = !isCollapsed;
-    setIsCollapsed(nextIsCollapsed);
-    localStorage.setItem(weekKey, JSON.stringify(nextIsCollapsed));
-  };
-
   return (
     <div>
       <div
-        onClick={changeCollapseStatus}
+        onClick={props.toggleIsCollapsed}
         style={{ alignItems: 'center', cursor: 'pointer', display: 'flex' }}
       >
         <h4 style={{ flexGrow: 1 }}>
-          <span>{isCollapsed ? '☞' : '☟'}</span> Week {props.week.number}
+          <span>{props.isCollapsed ? '☞' : '☟'}</span> Week {props.week.number}
         </h4>
         <DistanceComponent distance={props.week.totalDistance} symbol={totalDistanceSymbol} />
         <Time seconds={props.week.totalSeconds} />
       </div>
-      {!isCollapsed && (
+      {!props.isCollapsed && (
         <div
           className="week"
           style={{ display: 'flex', flexDirection: props.isDesktop ? 'row' : 'column' }}
