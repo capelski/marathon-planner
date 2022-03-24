c917a6d02dd31133762c15f01d727fb768979302
Carles Capellas <capellas.carles@gmail.com>
2022-03-24 13:49:16 +0100

    Display seconds for times under an hour

--- source/components/time.tsx
+++ source/components/time.tsx
@@ -7,11 +7,15 @@ interface TimeProps {
 
 export const Time: React.FC<TimeProps> = (props) => {
   const hours = Math.floor(props.seconds / 3600);
-  const minutes = Math.floor((props.seconds - hours * 3600) / 60);
+  const hoursRemainder = props.seconds - hours * 3600;
+  const minutes = Math.floor(hoursRemainder / 60);
+  const minutesReminder = hoursRemainder - minutes * 60;
+  const seconds = Math.floor(minutesReminder);
 
   return (
     <span style={{ paddingLeft: 4 }}>
-      {timeSymbol} {hours > 0 ? hours + 'h' : undefined} {minutes}'
+      {timeSymbol} {hours > 0 ? `${hours}h ` : undefined}
+      {minutes}'{hours === 0 && seconds > 0 ? ` ${seconds}"` : undefined}
     </span>
   );
 };
