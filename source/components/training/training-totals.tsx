86a373199166da113fe1e685e9fb2315fe7a5a0e
Carles Capellas <capellas.carles@gmail.com>
2023-08-28 20:12:11 +0200

    fix: add missing symbol

--- source/components/training/training-totals.tsx
+++ source/components/training/training-totals.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { totalDistanceSymbol } from '../../constants';
 import { PacedDistance } from '../../types';
 import { DistanceComponent } from '../distance';
 
@@ -9,7 +10,7 @@ interface TrainingTotalsProps {
 export const TrainingTotals: React.FC<TrainingTotalsProps> = (props) => {
   return (
     <div style={{ fontStyle: 'italic', marginTop: 8 }}>
-      <DistanceComponent distance={props.distance} />
+      <DistanceComponent distance={props.distance} symbol={totalDistanceSymbol} />
     </div>
   );
 };
